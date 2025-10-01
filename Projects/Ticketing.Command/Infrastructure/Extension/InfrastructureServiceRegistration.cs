94fbac708735d4c49714327279b842ad1505eb9d
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-10-01 17:46:06 -0600

    feature: implement infrastructure services and MongoDB configuration

---
+++ Projects/Ticketing.Command/Infrastructure/Extension/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Common.Core.Events;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Ticketing.Command.Domain.Abstracts;
+using Ticketing.Command.Infrastructure.Repositories;
+
+namespace Ticketing.Command.Infrastructure.Extension
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            BsonClassMap.RegisterClassMap<BaseEvent>();
+            BsonClassMap.RegisterClassMap<TicketCreatedEvent>();
+
+            services.AddScoped(typeof(IMongoRepository<>), typeof(MongoRepository<>));
+
+
+            services.AddTransient<IEventModelRepository, EventModelRepository>();
+            services.AddSingleton<IMongoClient, MongoClient>(
+                cfg => new MongoClient(configuration.GetConnectionString("MongoDb"))
+            ); // Unique connection to database
+
+
+            return services;
+
+        }
+    }
+}
--- Projects/Ticketing.Command/Infrastructure/Repositories/EventModelRepository.cs
+++ Projects/Ticketing.Command/Infrastructure/Repositories/EventModelRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace Ticketing.Command.Infrastructure.Repositories
+﻿using MongoDB.Driver;
+using Ticketing.Command.Domain.Abstracts;
+using Ticketing.Command.Domain.Models;
+
+namespace Ticketing.Command.Infrastructure.Repositories
 {
-    public class EventModelRepository
+    public class EventModelRepository : IEventModelRepository
     {
+        public IQueryable<EventModel> AsQueryable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IClientSessionHandle> BeginSessionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginTransaction(IClientSessionHandle session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitTransactionAsync(IClientSessionHandle session, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeSession(IClientSessionHandle session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EventModel> InsertOneAsync(EventModel document, IClientSessionHandle clientSesionHandle, CancellationToken cancellation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RollBackTransactionAsync(IClientSessionHandle session, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
